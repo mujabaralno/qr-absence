c0c2d29dfc9da985e9d7e0595e10026093b3d639
mujabaralno <jabarhah@example.com>
2025-05-01 14:42:11 +0700

    fix api webhook clerk role, aprrove

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -50,79 +50,78 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-    // CREATE
-    if (eventType === "user.created") {
-      const {
-        id,
-        email_addresses,
-        image_url,
-        first_name,
-        last_name,
-        public_metadata,
-      } = evt.data;
+  // CREATE
+  if (eventType === "user.created") {
+    const {
+      id,
+      email_addresses,
+      image_url,
+      first_name,
+      last_name,
+      public_metadata,
+    } = evt.data;
 
-      const organizationId = public_metadata?.organizationId as string;
-      const role = (public_metadata?.role as string) || "user";
-      const approved = (public_metadata?.approved as boolean) || false;
+    const organizationId = public_metadata?.organizationId as string;
+    const role = public_metadata?.role as string;
+    const approved = public_metadata?.approved as boolean;
 
-      const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        firstName: first_name!,
-        lastName: last_name!,
-        photo: image_url,
-        approved,
-        role,
-        organizationId
-      };
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      firstName: first_name!,
+      lastName: last_name!,
+      photo: image_url,
+      approved,
+      role,
+      organizationId,
+    };
 
-      try {
-        const newUser = await createUser(user);
-        console.log("User successfully created in DB:", newUser);
-        if (newUser) {
-          const result = await (
-            await clerkClient()
-          ).users.updateUserMetadata(id, {
-            publicMetadata: { userId: newUser._id, ...public_metadata },
-          });
-          console.log("New user created:", result);
-        }
-        return NextResponse.json({ message: "OK", user: newUser });
-      } catch (err) {
-        console.error("🔥 Failed to create user:", err);
-        return new Response("Error occurred during user creation", {
-          status: 500,
+    try {
+      const newUser = await createUser(user);
+      console.log("User successfully created in DB:", newUser);
+      if (newUser) {
+        const result = await (
+          await clerkClient()
+        ).users.updateUserMetadata(id, {
+          publicMetadata: { userId: newUser._id, ...public_metadata },
         });
+        console.log("New user created:", result);
       }
+      return NextResponse.json({ message: "OK", user: newUser });
+    } catch (err) {
+      console.error("🔥 Failed to create user:", err);
+      return new Response("Error occurred during user creation", {
+        status: 500,
+      });
     }
+  }
 
-    // UPDATE
-    if (eventType === "user.updated") {
-      const {
-        id: updatedUserId,
-        image_url,
-        first_name,
-        last_name,
-        public_metadata,
-      } = evt.data;
-      const approved = (public_metadata?.approved as boolean) || false;
-      
-      const user = {
-        firstName: first_name!,
-        lastName: last_name!,
-        photo: image_url,
-        approved,
+  // UPDATE
+  if (eventType === "user.updated") {
+    const {
+      id: updatedUserId,
+      image_url,
+      first_name,
+      last_name,
+      public_metadata,
+    } = evt.data;
+    const approved = (public_metadata?.approved as boolean) || false;
 
-      };
+    const user = {
+      firstName: first_name!,
+      lastName: last_name!,
+      photo: image_url,
+      approved,
+    };
 
-      try {
-        const updatedUser = await updateUser(updatedUserId, user);
-        return NextResponse.json({ message: "OK", user: updatedUser });
-      } catch (err) {
-        console.error("Error during user update:", err);
-        return new Response("Error occurred during user update", { status: 500 });
-      }
+    try {
+      const updatedUser = await updateUser(updatedUserId, user);
+      return NextResponse.json({ message: "OK", user: updatedUser });
+    } catch (err) {
+      console.error("Error during user update:", err);
+      return new Response("Error occurred during user update", { status: 500 });
     }
+  }
 
   // DELETE
   if (eventType === "user.deleted") {
